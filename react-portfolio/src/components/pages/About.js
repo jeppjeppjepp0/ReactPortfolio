140eace5df3ba4498e88aad1f7fe2343e12ac28d
jeppjeppjepp0 <jeddmj1@gmail.com>
2023-05-31 01:16:11 -0600

    basic structure fixed

---
+++ react-portfolio/src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+        <h1>About Page</h1>
+        <p>
+            I am a 20 year old currently living in Parker, CO, just 30 minutes south of Denver. I currently intern at the bilingual church Calvary Familia as a worship 
+            leader. I have played the cello for about 8 years and play the guitar and bass completely self-taught. I am currently a student enrolled in the UC Berkeley 
+            coding bootcamp.
+        </p>
+    </div>
+  );
+}
